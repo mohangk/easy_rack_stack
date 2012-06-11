980eaf3524e9984b3d897025886ae8c5ea62d1ff
Mohan Krishnan <pair+mohan@newcontext.com>
2012-06-11 15:11:40 +0800

    removed reference to readme for now

--- site-cookbooks/rails_app/metadata.rb
+++ site-cookbooks/rails_app/metadata.rb
@@ -2,7 +2,6 @@ maintainer        "Newcontext, PLC."
 maintainer_email  "amerson@newcontext.com / mohan@newcontext.com"
 license           "Apache 2.0"
 description       "Gets your Rails app up and running"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 recipe            "default", "Get your Rails freak on"
 
