223384c6cd3460fb4aa9ca08738a3f4e26d1fa97
Amerson Lin & Mohan Krishnan <pair+amerson+mohan@newcontext.com>
2012-06-18 12:08:30 +0800

    Setup of remote git repo is done without sudo

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -1,5 +1,5 @@
 git remote rm ec2
 git remote add ec2 ssh://nctx@$1/home/nctx/Pie/current
-git deploy setup -r ec2
+git deploy setup -s false -r ec2
 git deploy init
 git push ec2 pie_in_a_box:master
--- setup_vagrant.sh
+++ setup_vagrant.sh
@@ -1,5 +1,5 @@
 git remote rm vagrant
 git remote add vagrant ssh://nctx@localhost:2222/home/nctx/Pie/current
-git deploy setup -r vagrant 
+git deploy setup -s false -r vagrant 
 git deploy init
 git push vagrant pie_in_a_box:master
